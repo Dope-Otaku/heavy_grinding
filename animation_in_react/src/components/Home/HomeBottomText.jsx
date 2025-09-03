31ed33eebe7058149ece5ae14e1ed6e823758913
Dope-Otaku <fakeyt909@gmail.com>
2025-09-04 00:08:42 +0530

    fixed the bottom space issue in bottom text home ares

--- animation_in_react/src/components/Home/HomeBottomText.jsx
+++ animation_in_react/src/components/Home/HomeBottomText.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Projects from '../../pages/Projects'
+import Agents from '../../pages/Agents'
 
 const HomeBottomText = () => {
   return (
     <div className='font-[fontLarge] flex items-center justify-center gap-5'>
-        <Link className='text-[6.5vw] border-3 border-white rounded-full px-10 pt-1 pb-0 uppercase overflow-hidden'>Projects</Link>
-        <Link className='text-[6.5vw] border-3 border-white rounded-full px-10 pt-1 pb-0 uppercase overflow-hidden'>Agents</Link>
+        <Link to={Projects} className='text-[7vw] border-3 border-white rounded-full px-10 pt-5 mb-2 uppercase overflow-hidden leading-[5vw]'>Projects</Link>
+        <Link to={Agents} className='text-[7vw] border-3 border-white rounded-full px-10 pt-5 mb-2 uppercase overflow-hidden leading-[5vw]'>Agents</Link>
     </div>
   )
 }
