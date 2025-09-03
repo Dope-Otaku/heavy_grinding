f593b8f8f312086409fa6865591cfc807ad08c44
Dope-Otaku <fakeyt909@gmail.com>
2025-09-04 00:29:47 +0530

    some minor changes in bottom text hover image

--- animation_in_react/src/components/Home/HomeBottomText.jsx
+++ animation_in_react/src/components/Home/HomeBottomText.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
-import Projects from '../../pages/Projects'
-import Agents from '../../pages/Agents'
 
 const HomeBottomText = () => {
   return (
     <div className='font-[fontLarge] flex items-center justify-center gap-5'>
-        <Link to={Projects} className='text-[7vw] border-3 border-white rounded-full px-10 pt-5 mb-2 uppercase overflow-hidden leading-[5vw]'>Projects</Link>
-        <Link to={Agents} className='text-[7vw] border-3 border-white rounded-full px-10 pt-5 mb-2 uppercase overflow-hidden leading-[5vw]'>Agents</Link>
+        <Link to='/projects' className='text-[7vw] hover:border-[#d3fd50] hover:text-[#d3fd50] border-3 border-white rounded-full px-10 pt-5 mb-2 uppercase overflow-hidden leading-[5vw]'>Projects</Link>
+        <Link to='/agents' className='text-[7vw] hover:border-[#d3fd50] hover:text-[#d3fd50] border-3 border-white rounded-full px-10 pt-5 mb-2 uppercase overflow-hidden leading-[5vw]'>Agents</Link>
     </div>
   )
 }
