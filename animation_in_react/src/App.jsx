f6021ba2cfc2d8b6fae85663ac680d0c59abdcda
Dope-Otaku <fakeyt909@gmail.com>
2025-09-03 21:56:30 +0530

    we have installed react router and tested it

--- animation_in_react/src/App.jsx
+++ animation_in_react/src/App.jsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import Agents from "./pages/Agents"
 
 function App() {
 
+
   return (
     <>
-      <h1 className="text-6xl">hello world</h1>
+      <h1 className="text-6xl text-black">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/agents" element={<Agents/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+        </Routes>
+      </h1>
     </>
   )
 }
