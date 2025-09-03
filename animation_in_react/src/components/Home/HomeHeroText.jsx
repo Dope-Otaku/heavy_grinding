9d8a4105e201a272d1b2c7419bc1a73e0ba4c0f9
Dope-Otaku <fakeyt909@gmail.com>
2025-09-03 23:12:37 +0530

    added these text which will be decorated in home hero section

---
+++ animation_in_react/src/components/Home/HomeHeroText.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HomeHeroText = () => {
+  return (
+    <div>
+        <div>
+            L'étincelle
+        </div>
+        <div>
+            quigénère
+        </div>
+        <div>
+            la créativité
+        </div>
+    </div>
+  )
+}
+
+export default HomeHeroText
