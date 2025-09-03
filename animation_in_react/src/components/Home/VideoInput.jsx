d6d6cdac3defd533988a683084708a823b4c552c
Dope-Otaku <fakeyt909@gmail.com>
2025-09-03 22:57:09 +0530

    we have used a public video stored in env and using in home video section through components

--- animation_in_react/src/components/Home/VideoInput.jsx
+++ animation_in_react/src/components/Home/VideoInput.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const VideoInput = () => {
+
+  const videoSource = import.meta.env.VITE_HomeVideo //currently i am using a public url
+  console.log(videoSource)
   return (
-    <div>VideoInput</div>
+    <div className='h-full w-full'>
+      <video className='h-full w-full object-cover' autoPlay muted loop src={videoSource}></video>
+    </div>
   )
 }
 
--- animation_in_react/src/pages/Home.jsx
+++ animation_in_react/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import VideoInput from '../components/Home/VideoInput'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='h-screen w-screen bg-red-900'>
+        <VideoInput/>
+    </div>
   )
 }
 
