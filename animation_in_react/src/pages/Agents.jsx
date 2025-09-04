f01aca658e55cd6ec56132dcb3e5fe6429b759e2
Dope-Otaku <fakeyt909@gmail.com>
2025-09-04 21:00:02 +0530

    beautifully configured the text for agents hero section

--- animation_in_react/src/pages/Agents.jsx
+++ animation_in_react/src/pages/Agents.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Agents = () => {
   return (
-    <div>Agents</div>
+    <div className='font-[fontLarge]'>
+      <div className='mt-[56vh]'>
+        <h1 className='text-[20vw] text-center uppercase leading-[17.3vw]'>Soixan7e<br/>Douze</h1>
+      </div>
+    </div>
   )
 }
 
