bd974035878c0e25790f5b150814362fcc167d02
Dope-Otaku <fakeyt909@gmail.com>
2025-09-04 21:14:36 +0530

    completed the para below the hero section with custom space

--- animation_in_react/src/pages/Agents.jsx
+++ animation_in_react/src/pages/Agents.jsx
@@ -6,6 +6,9 @@ const Agents = () => {
       <div className='mt-[56vh]'>
         <h1 className='text-[20vw] text-center uppercase leading-[17.3vw]'>Soixan7e<br/>Douze</h1>
       </div>
+      <div className='bg-red-500 pl-[40%]'>
+        <p className='text-[6xl]'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+      </div>
     </div>
   )
 }
