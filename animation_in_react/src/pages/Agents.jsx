76bf4315d3d61512c0aa2b3347d2a7f6c5f34882
Dope-Otaku <fakeyt909@gmail.com>
2025-09-05 23:33:30 +0530

    completed the structure and also formatted the image position

--- animation_in_react/src/pages/Agents.jsx
+++ animation_in_react/src/pages/Agents.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 
 const Agents = () => {
   return (
-    <div className='font-[fontLarge]'>
-      <div className='mt-[56vh]'>
-        <h1 className='text-[20vw] text-center uppercase leading-[17.3vw]'>Soixan7e<br/>Douze</h1>
+    <div>
+      <div className='section1'>
+        <div className='absolute overflow-hidden object-cover h-[20vw] w-[15vw]  top-60 left-[30vw] rounded-3xl bg-red-500'>
+          <img src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
+        </div>
+        <div className=' relative font-[fontLarge]'>
+          <div className='mt-[56vh]'>
+            <h1 className='text-[20vw] text-center uppercase leading-[17.3vw]'>Soixan7e<br/>Douze</h1>
+          </div>
+          <div className='pl-[40%]'>
+            <p className='text-[6xl]'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+          </div>
+        </div>
       </div>
-      <div className='bg-red-500 pl-[40%]'>
-        <p className='text-[6xl]'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+      <div className="section2">
       </div>
     </div>
   )
