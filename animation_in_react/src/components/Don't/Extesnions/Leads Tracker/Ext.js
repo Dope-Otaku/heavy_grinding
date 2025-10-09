d5b45c6d177ed3872e9d5291d00816ed08b272dc
Dope-Otaku <fakeyt909@gmail.com>
2025-10-09 15:06:44 +0530

    tried a new method also addeventlisteneer

--- animation_in_react/src/components/Don't/Extesnions/Leads Tracker/Ext.js
+++ animation_in_react/src/components/Don't/Extesnions/Leads Tracker/Ext.js
@@ -1,5 +1,9 @@
 const saveInput = document.getElementById('input-btn')
 
-function inputSaved(){
+// function inputSaved(){
+//     console.log("save button clicked!")
+// }
+
+saveInput.addEventListener('click', ()=>{
     console.log("save button clicked!")
-}
+})
