6d18d3f479dbccb3d9afbb8b24e25839fd582349
Dope-Otaku <fakeyt909@gmail.com>
2025-10-09 16:11:52 +0530

    hands on local storage

--- animation_in_react/src/components/Don't/Extesnions/Leads Tracker/Ext.js
+++ animation_in_react/src/components/Don't/Extesnions/Leads Tracker/Ext.js
@@ -1,8 +1,10 @@
 const saveInput = document.getElementById('input-btn')
 const showInput = document.getElementById('input-btn2')
+const clearStorage = document.getElementById('input-btn3')
 const inputFeild = document.getElementById('input-el')
 const itemsClass = document.getElementById('items-class')
 let myLeads = []
+let count = 0
 
 
 
@@ -16,6 +18,8 @@ let myLeads = []
 saveInput.addEventListener('click', ()=>{
     let input = inputFeild.value
     myLeads.push(input)
+    count +=1
+    localStorage.setItem(`${count}`,`${input}`)
     console.log(input)
     inputFeild.value = ""
 })
@@ -24,8 +28,13 @@ showInput.addEventListener('click', ()=>{
     let listItems = ""
 
     for(let i=0; i<myLeads.length; i++){
-        listItems += "<li>" + myLeads[i] + "</li>"
-        itemsClass.innerHTML = listItems
-        console.log(myLeads[i])
+        listItems += "<li>" + `<a href=${myLeads[i]} target='_blank'>` + myLeads[i] +"</a>" + "</li>"
+        itemsClass.innerHTML += listItems
+        console.log(localStorage.getItem(`${i}`))
+        // console.log(myLeads[i])
     }
+})
+
+clearStorage.addEventListener('click', ()=>{
+    localStorage.clear()
 })
