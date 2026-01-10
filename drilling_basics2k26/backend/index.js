33de7d797eecc569dcc9dbdb63eab11345ee9795
Dope-Otaku <fakeyt909@gmail.com>
2026-01-10 16:43:36 +0530

    testing out env file

--- drilling_basics2k26/backend/index.js
+++ drilling_basics2k26/backend/index.js
@@ -1,5 +1,14 @@
 import app from "./server.js"
+import mongodb from "mongodb"
+// import reviewsDAO from "./dao/reviewsDAO.js"
+
+
+
+
+const mongoClient = mongodb.MongoClient
+const mongo_username = process.env.MONGO_USERNAME
+
 
 app.listen(5000, ()=>{
-    console.log("server is active")
+    console.log(`server is active: ${mongo_username}`)
 })
