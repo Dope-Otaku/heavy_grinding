95f1d4f2128a2d10012bf8f36f3ff9afcd433f04
Souvik Halder <fakeyt909@gmail.com>
2026-01-12 16:25:40 +0530

    added all the routes connected with backed and ready to talk with database

--- drilling_basics2k26/backend/api/reviews.route.js
+++ drilling_basics2k26/backend/api/reviews.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-// import ReviewsCtrl from "./reviews.controller.js"
+import ReviewsCtrl from "./reviews.controller.js"
 
 
 const router = express.Router()
@@ -15,5 +15,11 @@ router.route(/(.*)/).get((req, res)=>{
     res.status(404).json({error: "pitched in wrong page"})
 })
 
+router.route("/movie/:id").get(ReviewsCtrl.apiGetReviews)
+router.route("/new").post(ReviewsCtrl.apiPostReview)
+router.route("/:id")
+    .get(ReviewsCtrl.apiGetReview)
+    .put(ReviewsCtrl.apiUpdateReview)
+    .delete(ReviewsCtrl.apiDeleteReview)
 
 export default router
