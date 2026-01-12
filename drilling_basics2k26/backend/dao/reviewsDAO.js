c4aeae58f98aed1c1e42a0c0fe608d47c6415221
Souvik Halder <fakeyt909@gmail.com>
2026-01-12 16:49:12 +0530

    added addReview in reviewsDAO file

--- drilling_basics2k26/backend/dao/reviewsDAO.js
+++ drilling_basics2k26/backend/dao/reviewsDAO.js
@@ -17,4 +17,18 @@ export default class ReviewsDAO{
             console.error(`Unable to establish Data Collection handles : ${e}`)
         }
     }
+
+    static async addReview(movieId, review, user){
+        try {
+            const reviewDoc = {
+                movieId: movieId,
+                review: review,
+                user: user
+            }
+            return await reviews.insertOne(reviewDoc)
+        } catch (e) {
+            console.error(`Unable to Post review : ${e}`)
+            return {error: e}
+        }
+    }
 }
