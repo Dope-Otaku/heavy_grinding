93f2eb4d9563a7a540cc3ef268687a4d689e14cf
Souvik Halder <fakeyt909@gmail.com>
2026-01-12 16:41:43 +0530

    added injectDB function in ReviewDAO

--- drilling_basics2k26/backend/dao/reviewsDAO.js
+++ drilling_basics2k26/backend/dao/reviewsDAO.js
@@ -1,8 +1,20 @@
+import mongodb from 'mongodb'
+const ObjectId = mongodb.ObjectId
 
-function reviewsDAO() {
-  return (
-    console.log("hello")
-  )
+
+
+let reviews
+
+
+export default class ReviewsDAO{
+    static async injectDB(conn){
+        if(reviews){
+            return
+        }
+        try {
+            await conn.db("reviews").collection("reviews")
+        } catch (e) {
+            console.error(`Unable to establish Data Collection handles : ${e}`)
+        }
+    }
 }
-
-export default reviewsDAO
