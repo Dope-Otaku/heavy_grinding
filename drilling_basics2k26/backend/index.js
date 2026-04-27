2158de7469b062813a7c759385d0516671bfdc36
Souvik Halder <fakeyt909@gmail.com>
2026-04-27 23:21:22 +0530

    bug: solved for server start

--- drilling_basics2k26/backend/index.js
+++ drilling_basics2k26/backend/index.js
@@ -32,6 +32,7 @@ const connectDatabase = async() =>{
         // const db = await client.db("reviews").collection("reviews")
         // console.log(`${db} : connected with the table ${mongo_db}:${db}`)
         // console.log(`${db} : connected with the table ${mongo_db}:${db}`)
+        // console.log(`${db} : connected with the table ${mongo_db}:${db}`)
 
         await ReviewsDAO.injectDB(client)
 
